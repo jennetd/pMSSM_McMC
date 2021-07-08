ed990bdaa9b692849533fe9be3c53089bed30604
Jennet Dickinson <jdickinson@lbl.gov>
2021-07-08 17:17:17 +0200

    update superiso slha.c

--- packages/slha.c
+++ packages/slha.c
@@ -52,45 +52,47 @@ int main(int argc,char** argv)
 		printf("BR(Bd->mu mu)\t\t\t%.3e\n\n",Bdmumu_calculator(name));
 	
 		printf("BR(B->K* mu mu)_low\t\t%.3e\n",BRobs_BKstarmumu_lowq2_calculator(name,obs));
-		printf("AFB(B->K* mu mu)_low\t\t%.3e\n",obs[1]);
-		printf("FL(B->K* mu mu)_low\t\t%.3e\n",obs[2]);
-		printf("P1(B->K* mu mu)_low\t\t%.3e\n",obs[5]);
-		printf("P2(B->K* mu mu)_low\t\t%.3e\n",obs[14]);
-		printf("P4'(B->K* mu mu)_low\t\t%.3e\n",obs[17]);
-		printf("P5'(B->K* mu mu)_low\t\t%.3e\n",obs[18]);
-		printf("P6'(B->K* mu mu)_low\t\t%.3e\n",obs[19]);
-		printf("P8'(B->K* mu mu)_low\t\t%.3e\n",obs[21]);
-		printf("AI(B->K* mu mu)_low\t\t%.3e\n\n",AI_BKstarmumu_lowq2_calculator(name));
+		//		printf("AFB(B->K* mu mu)_low\t\t%.3e\n",obs[1]);
+		//		printf("FL(B->K* mu mu)_low\t\t%.3e\n",obs[2]);
+		//		printf("P1(B->K* mu mu)_low\t\t%.3e\n",obs[5]);
+		//		printf("P2(B->K* mu mu)_low\t\t%.3e\n",obs[14]);
+		//		printf("P4'(B->K* mu mu)_low\t\t%.3e\n",obs[17]);
+		//		printf("P5'(B->K* mu mu)_low\t\t%.3e\n",obs[18]);
+		//		printf("P6'(B->K* mu mu)_low\t\t%.3e\n",obs[19]);
+		//		printf("P8'(B->K* mu mu)_low\t\t%.3e\n",obs[21]);
+		//		printf("AI(B->K* mu mu)_low\t\t%.3e\n\n",AI_BKstarmumu_lowq2_calculator(name));
 	
 		printf("BR(B->K* mu mu)_high\t\t%.3e\n",BRobs_BKstarmumu_highq2_calculator(name,obs));
-		printf("AFB(B->K* mu mu)_high\t\t%.3e\n",obs[1]);
-		printf("FL(B->K* mu mu)_high\t\t%.3e\n",obs[2]);
-		printf("P1(B->K* mu mu)_high\t\t%.3e\n",obs[5]);
-		printf("P2(B->K* mu mu)_high\t\t%.3e\n",obs[14]);
-		printf("P4'(B->K* mu mu)_high\t\t%.3e\n",obs[17]);
-		printf("P5'(B->K* mu mu)_high\t\t%.3e\n",obs[18]);
-		printf("P6'(B->K* mu mu)_high\t\t%.3e\n",obs[19]);
-		printf("P8'(B->K* mu mu)_high\t\t%.3e\n",obs[21]);
-		printf("AI(B->K* mu mu)_high\t\t%.3e\n\n",AI_BKstarmumu_highq2_calculator(name));
+		//		printf("AFB(B->K* mu mu)_high\t\t%.3e\n",obs[1]);
+		//		printf("FL(B->K* mu mu)_high\t\t%.3e\n",obs[2]);
+		//		printf("P1(B->K* mu mu)_high\t\t%.3e\n",obs[5]);
+		//		printf("P2(B->K* mu mu)_high\t\t%.3e\n",obs[14]);
+		//		printf("P4'(B->K* mu mu)_high\t\t%.3e\n",obs[17]);
+		//		printf("P5'(B->K* mu mu)_high\t\t%.3e\n",obs[18]);
+		//		printf("P6'(B->K* mu mu)_high\t\t%.3e\n",obs[19]);
+		//		printf("P8'(B->K* mu mu)_high\t\t%.3e\n",obs[21]);
+		//		printf("AI(B->K* mu mu)_high\t\t%.3e\n\n",AI_BKstarmumu_highq2_calculator(name));
 
 		printf("BR(B->Xs e e)_low\t\t%.3e\n",BRBXsee_lowq2_calculator(name));
                 printf("BR(B->Xs e e)_high\t\t%.3e\n",BRBXsee_highq2_calculator(name));
+
 		printf("BR(B->Xs mu mu)_low\t\t%.3e\n",BRBXsmumu_lowq2_calculator(name));
 		printf("BR(B->Xs mu mu)_high\t\t%.3e\n",BRBXsmumu_highq2_calculator(name));
-		printf("q0^2(AFB(B->Xs mu mu)\t\t%.3e\n",A_BXsmumu_zero_calculator(name));
+		//		printf("q0^2(AFB(B->Xs mu mu)\t\t%.3e\n",A_BXsmumu_zero_calculator(name));
+
 		printf("BR(B->Xs tau tau)_high\t\t%.3e\n\n",BRBXstautau_highq2_calculator(name));
 	
 		printf("BR(B->tau nu)\t\t\t%.3e\n",Btaunu_calculator(name));
-		printf("R(B->tau nu)\t\t\t%.3e\n",RBtaunu_calculator(name));
+		//		printf("R(B->tau nu)\t\t\t%.3e\n",RBtaunu_calculator(name));
 		printf("BR(B->D tau nu)\t\t\t%.3e\n",BDtaunu_calculator(name));
-		printf("BR(B->D tau nu)/BR(B->D e nu)\t%.3e\n",BDtaunu_BDenu_calculator(name));
+		//		printf("BR(B->D tau nu)/BR(B->D e nu)\t%.3e\n",BDtaunu_BDenu_calculator(name));
 		printf("BR(Ds->tau nu)\t\t\t%.3e\n",Dstaunu_calculator(name));
 		printf("BR(Ds->mu nu)\t\t\t%.3e\n",Dsmunu_calculator(name));
 		printf("BR(D->mu nu)\t\t\t%.3e\n",Dmunu_calculator(name));
-		printf("BR(K->mu nu)/BR(pi->mu nu)\t%.3e\n",Kmunu_pimunu_calculator(name));
-		printf("Rmu23(K->mu nu)\t\t\t%.3e\n\n",Rmu23_calculator(name));
+		//		printf("BR(K->mu nu)/BR(pi->mu nu)\t%.3e\n",Kmunu_pimunu_calculator(name));
+		//		printf("Rmu23(K->mu nu)\t\t\t%.3e\n\n",Rmu23_calculator(name));
 
-		printf("a_muon\t\t\t\t%.3e\n\n",muon_gm2_calculator(name));
+		//		printf("a_muon\t\t\t\t%.3e\n\n",muon_gm2_calculator(name));
 
  		if(test==3)
 		{ 	
